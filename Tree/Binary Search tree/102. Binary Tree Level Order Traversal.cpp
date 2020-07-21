21364600efcc3a28ba1c1e5e0f1cc15f43165c90
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-07-21 14:06:54 +0530

    BT

---
+++ Tree/Binary Search tree/102. Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,79 @@
+102. Binary Tree Level Order Traversal
+Medium
+
+Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its level order traversal as:
+[
+  [3],
+  [9,20],
+  [15,7]
+]
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* node) {
+        vector<vector<int>> v;
+        if(node==NULL)
+        {
+            return {};
+        }
+        
+        queue<TreeNode*> que;
+        que.push(node);
+
+        //int level = 0;
+        while (que.size() != 0)
+        {
+            int size = que.size();
+            //cout << "Level: " << level << " -> ";
+            vector<int> ans;
+
+            while (size-- > 0)
+            {
+                TreeNode *currNode = que.front();
+                que.pop();
+                
+
+                //cout << currNode->data << ", ";
+                ans.push_back(currNode->val);
+
+                if (currNode->left != NULL)
+                {
+                    que.push(currNode->left);
+                }
+
+                if (currNode->right != NULL)
+                {
+                    que.push(currNode->right);
+                }
+            }
+            v.push_back(ans);
+
+        //level++;
+        //cout << "\n";
+        }
+    //cout << "\n";
+        return v;
+        
+    }
+};
+
