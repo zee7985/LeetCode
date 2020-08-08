464980876909fce12ff4a5da4301cb1341fa298f
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-08-08 13:10:35 +0530

    Medium

---
+++ Tree/Binary Search tree/173. Binary Search Tree Iterator.cpp
@@ -0,0 +1,71 @@
+173. Binary Search Tree Iterator
+
+Implement an iterator over a binary search tree (BST). Your iterator will be initialized with the root node of a BST.
+
+Calling next() will return the next smallest number in the BST.
+
+ 
+
+Example:
+
+
+
+BSTIterator iterator = new BSTIterator(root);
+iterator.next();    // return 3
+iterator.next();    // return 7
+iterator.hasNext(); // return true
+iterator.next();    // return 9
+iterator.hasNext(); // return true
+iterator.next();    // return 15
+iterator.hasNext(); // return true
+iterator.next();    // return 20
+iterator.hasNext(); // return false
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    stack<TreeNode*> st;
+    BSTIterator(TreeNode* root) {
+        rightKeLeft(root);
+    }
+    
+    void rightKeLeft(TreeNode * root)
+    {
+        while(root!=NULL)
+        {
+            st.push(root);
+            root=root->left;
+        }
+        
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        TreeNode * curr= st.top(); st.pop();
+        rightKeLeft(curr->right);
+        return curr->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return st.size()!=0;
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+ 
