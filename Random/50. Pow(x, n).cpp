acfe55650385f32e3c2981b55dd13cca977c2988
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-09-25 14:04:07 +0530

    Create 50. Pow(x, n).cpp

---
+++ Random/50. Pow(x, n).cpp
@@ -0,0 +1,60 @@
+50. Pow(x, n)
+Medium
+
+Implement pow(x, n), which calculates x raised to the power n (i.e. xn).
+
+ 
+
+Example 1:
+
+Input: x = 2.00000, n = 10
+Output: 1024.00000
+Example 2:
+
+Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+ 
+
+Constraints:
+
+-100.0 < x < 100.0
+-231 <= n <= 231-1
+-104 <= xn <= 104
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans=1.0;
+        long long nn=n; 
+        
+        // For -ve edge case-2,147,483,648 to 2,147,483,647
+        if(nn<0)
+        {
+            nn=-1*nn;
+        }
+        
+        while(nn)
+        {
+            if(nn%2==0)
+            {
+                x=x*x;
+                nn=nn/2;
+            }
+            else
+            {
+                ans=ans*x;
+                nn=nn-1;
+            }
+        }
+        if(n<0)
+        {
+          return 1.0/ans;   //x^-n= 1/x^n  
+        }
+        return ans;
+    }
+};
