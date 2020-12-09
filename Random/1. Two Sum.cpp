c99a91e6036c4b781f07126e4fb8cfc9a435fd5f
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-12-09 20:03:26 +0530

    Update 1. Two Sum.cpp

--- Random/1. Two Sum.cpp
+++ Random/1. Two Sum.cpp
@@ -80,3 +80,58 @@ public:
     return {-1,-1};
     }
 };
+
+//================================================================
+2-Variation of 2sum
+
+Pairs of Songs With Total Durations Divisible by 60
+
+You are given a list of songs where the ith song has a duration of time[i] seconds.
+
+Return the number of pairs of songs for which their total duration in seconds is divisible by 60. Formally, we want the number of indices i, j such that i < j with (time[i] + time[j]) % 60 == 0.
+
+ 
+
+Example 1:
+
+Input: time = [30,20,150,100,40]
+Output: 3
+Explanation: Three pairs have a total duration divisible by 60:
+(time[0] = 30, time[2] = 150): total duration 180
+(time[1] = 20, time[3] = 100): total duration 120
+(time[1] = 20, time[4] = 40): total duration 60
+Example 2:
+
+Input: time = [60,60,60]
+Output: 3
+Explanation: All three pairs have a total duration of 120, which is divisible by 60.
+ 
+
+Constraints:
+
+1 <= time.length <= 6 * 104
+1 <= time[i] <= 500
+	
+class Solution {
+public:
+    //Similar to two sum
+    int numPairsDivisibleBy60(vector<int>& time) {
+        int ans=0;
+        map<int,int> mp;
+        for(auto t:time)
+        {
+            if(t%60==0)
+            {
+                ans+=mp[0];
+            }
+            else if(mp.find(60-t%60)!=mp.end())
+            {
+                ans+=mp[60- t%60];
+                
+            }
+            mp[t%60]++;
+            
+        }
+        return ans;
+    }
+};
