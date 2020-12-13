8f5ec81879597accf1830cd511bebaf663c780c5
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-12-13 16:16:50 +0530

    Pascal Triangle (Row+ Matrix)

--- Random/Pascal's Triangle II.cpp
+++ Random/Pascal's Triangle II.cpp
@@ -51,3 +51,44 @@ public:
         return ans;
     }
 };
+
+
+//2. Given numRows, generate the first numRows of Pascal’s triangle.
+
+// Pascal’s triangle : To generate A[C] in row R, sum up A’[C] and A’[C-1] from previous row R - 1.
+
+// Example:
+
+// Given numRows = 5,
+
+// Return
+
+// [
+//      [1],
+//      [1,1],
+//      [1,2,1],
+//      [1,3,3,1],
+//      [1,4,6,4,1]
+// ]
+
+
+vector<vector<int> > Solution::solve(int A) {
+    vector<vector<int>> dp(A);
+    for(int i=0;i<A;i++)
+    {
+        dp[i].resize(i+1);
+        for(int j=0;j<=i;j++)
+        {
+            if(j==0 || j==i)
+            {
+                dp[i][j]=1;
+            }
+            else 
+            {
+                dp[i][j]=dp[i-1][j] + dp[i-1][j-1];
+            }
+        }
+    }
+    return dp;
+}
+
