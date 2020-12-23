2c105754b2571c313833f49d1342b5d94b17f54a
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-12-23 16:33:58 +0530

    Smilar to 1695. Maximum Erasure Value

--- Random/3. Longest Substring Without Repeating Characters.cpp
+++ Random/3. Longest Substring Without Repeating Characters.cpp
@@ -92,3 +92,64 @@ public:
         
     }
 };
+
+
+
+1695. Maximum Erasure Value
+
+Difficulty:Medium
+You are given an array of positive integers nums and want to erase a subarray containing unique elements. The score you get by erasing the subarray is
+equal to the sum of its elements.
+
+Return the maximum score you can get by erasing exactly one subarray.
+
+An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
+
+ 
+
+Example 1:
+
+Input: nums = [4,2,4,5,6]
+Output: 17
+Explanation: The optimal subarray here is [2,4,5,6].
+Example 2:
+
+Input: nums = [5,2,1,2,5,2,1,2,5]
+Output: 8
+Explanation: The optimal subarray here is [5,2,1] or [1,2,5].
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 104
+ 
+ class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        int n=nums.size();
+        set<int> hs;
+        int max_=0;
+        int sum=0;
+       
+        int i=0,j=0;
+        while(i<n && j<n)
+        {
+            if(hs.find(nums[j])==hs.end())
+            {
+                hs.insert(nums[j]);
+                sum+=nums[j];
+                j++;
+                max_=max(max_,sum);
+    
+            }
+            else{
+                hs.erase(nums[i]);
+                sum-=nums[i];
+                i++;
+            }
+        }
+        return max_;
+        
+    }
+};
