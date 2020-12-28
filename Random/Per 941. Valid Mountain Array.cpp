d7aca62650a04aae0d6c94fb0c5924ad428c5273
Zeeshan <53146361+zee7985@users.noreply.github.com>
2020-12-28 14:09:14 +0530

    Easy

---
+++ Random/Per 941. Valid Mountain Array.cpp
@@ -0,0 +1,43 @@
+941. Valid Mountain Array
+Easy
+
+Given an array of integers arr, return true if and only if it is a valid mountain array.
+
+Recall that arr is a mountain array if and only if:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i]
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+
+ Example 1:
+
+Input: arr = [2,1]
+Output: false
+Example 2:
+
+Input: arr = [3,5,5]
+Output: false
+Example 3:
+
+Input: arr = [0,3,2,1]
+Output: true
+
+class Solution {
+public:    
+    bool validMountainArray(vector<int>&arr)
+    {
+        if( arr.size() < 3) return false;
+        
+        int i = 0;
+        bool flag1 = false , flag2 = false;
+        
+        if( arr[i] < arr[i+1] )
+        {
+            while( i+1 != arr.size() && arr[i] < arr[i+1] ) {i++; flag1 = true;}
+            while( i+1 != arr.size() && arr[i] > arr[i+1]) {i++; flag2 = true;}
+        }
+        if( i == arr.size()-1  && flag1 && flag2 ) return true;
+        return false;
+    }
+};
