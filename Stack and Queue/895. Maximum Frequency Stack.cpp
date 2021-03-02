43df43c1cd331f64811c6c9e84bec8f8aeac4ad3
Zeeshan <53146361+zee7985@users.noreply.github.com>
2021-03-02 14:59:41 +0530

    Hard - Map of stack

---
+++ Stack and Queue/895. Maximum Frequency Stack.cpp
@@ -0,0 +1,98 @@
+895. Maximum Frequency Stack
+Hard
+
+Implement FreqStack, a class which simulates the operation of a stack-like data structure.
+
+FreqStack has two functions:
+
+push(int x), which pushes an integer x onto the stack.
+pop(), which removes and returns the most frequent element in the stack.
+If there is a tie for most frequent element, the element closest to the top of the stack is removed and returned.
+ 
+
+Example 1:
+
+Input: 
+["FreqStack","push","push","push","push","push","push","pop","pop","pop","pop"],
+[[],[5],[7],[5],[7],[4],[5],[],[],[],[]]
+Output: [null,null,null,null,null,null,null,5,7,5,4]
+Explanation:
+After making six .push operations, the stack is [5,7,5,7,4,5] from bottom to top.  Then:
+
+pop() -> returns 5, as 5 is the most frequent.
+The stack becomes [5,7,5,7,4].
+
+pop() -> returns 7, as 5 and 7 is the most frequent, but 7 is closest to the top.
+The stack becomes [5,7,5,4].
+
+pop() -> returns 5.
+The stack becomes [5,7,4].
+
+pop() -> returns 4.
+The stack becomes [5,7].
+ 
+
+Note:
+
+Calls to FreqStack.push(int x) will be such that 0 <= x <= 10^9.
+It is guaranteed that FreqStack.pop() won't be called if the stack has zero elements.
+The total number of FreqStack.push calls will not exceed 10000 in a single test case.
+The total number of FreqStack.pop calls will not exceed 10000 in a single test case.
+The total number of FreqStack.push and FreqStack.pop calls will not exceed 150000 across all test cases.
+
+
+//[[],[5],[7],[5],[7],[4],[5],[],[],[],[]]
+//  Push element into map of stack where freq is the key and stack of numbers pushed at that frequency as value
+        
+//     FreqStack                              freqMAp
+    
+//     freq     stack                         Ele     Freq
+//     1   ->    4                             5  ->   3
+//               7
+//               5                             7  ->   2
+        
+//     2   ->    7                             4  ->   1
+//               5
+        
+//     3   ->    5
+        
+//     MaxFreq=3;
+    
+//     While popping : 
+//     Return and remove the top element from the stack where key == maxFreq
+//     If stack become empty reduce maxFreq
+//     Reduce count from freqMap
+        
+class FreqStack {
+public:
+   
+    
+    unordered_map<int, int> freq;
+    unordered_map<int, stack<int>> m;
+    int maxfreq = 0;
+    FreqStack() {
+     }
+
+    void push(int x) {
+        freq[x]++;
+        maxfreq = max(maxfreq,freq[x]);
+        m[freq[x]].push(x);
+    }
+
+    int pop() {
+        int x = m[maxfreq].top();
+        m[maxfreq].pop();
+        if (m[freq[x]--].size()==0) 
+        {
+            maxfreq--;
+        }
+        return x;
+    }
+};
+
+/**
+ * Your FreqStack object will be instantiated and called as such:
+ * FreqStack* obj = new FreqStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ */
